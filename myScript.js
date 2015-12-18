ddb6a2565c4a73efcb198b20c6c6fd183527beef
strint <xiaoyulink@gmail.com>
2015-12-18 22:45:20 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,51 +1,13 @@
 function setup(){
-  var myCanvas = createCanvas(710, 400, WEBGL);
+  var myCanvas = createCanvas(710, 400);
   myCanvas.parent('p5Container');
 }
 
-function draw(){
-  background(250);
-  translate(-250 * 2.5, 0, 0);
-  normalMaterial();
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  plane(80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(80, 80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cylinder(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cone(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(80, 20);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  sphere(80);
-  pop();
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
