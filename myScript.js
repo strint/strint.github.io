358421dbf1b6adc5ea8cee6b774d524dcc2f036d
strint <xiaoyulink@gmail.com>
2016-01-26 15:30:48 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -5,9 +5,10 @@ function setup(){
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
-  } else {
     fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  } 
+  //else {
+  //  fill(255);
+  //}
+  ellipse(mouseX, mouseY, 40, 40);
 }
